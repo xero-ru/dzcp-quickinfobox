11ecb446d5688f4dfcb9ed1ed8817fe92798f6a0
Misha Plot <mishaplot@mail.ru>
2017-12-18 23:07:00 +0100

    update deutsch

---
+++ PHP/inc/additional-languages/english/quickinfo.php
@@ -0,0 +1,18 @@
+<?php
+define('_config_quickinfo', 'Quickinfo');
+define('_quickinfo_more', 'more');
+define('_quickinfo_head', 'Quickinfo - AddOn by <a href="https://github.com/BlueTeck/dzcp-quickinfobox/">BlueTeck</a>');
+define('_quickinfo_add_head', 'Add new Quickinfo');
+define('_quickinfo_edit_head', 'Quickinfo edit');
+define('_quickinfo_added', 'Add Quickinfo');
+define('_quickinfo_deleted', 'Quikinfo deleted');
+define('_quickinfo_titel', 'Quickinfo-Title');
+define('_quickinfo_admin_show_set', 'This tool tip to visible');
+define('_quickinfo_admin_show_unset', 'This tooltip turn invisible');
+define('_quickinfo_admin_status_setted', 'The tooltip is now visible!');
+define('_quickinfo_admin_status_unsetted', 'The tooltip is now invisible!');
+define('_confirm_del_quickinfo', 'really want to delete?');
+define('_quickinfo_edited', 'Quickinfo edited');
+define('_quickinfo_more_link', 'related link');
+define('_perm_quickinfo', 'Quickinfo Management');
+?>
---
+++ PHP/inc/additional-languages/russian/quickinfo.php
@@ -0,0 +1,21 @@
+﻿<?php
+#########################################################################
+####         Translated by www.russian-speznas.com /// xero.ru       ####            
+#########################################################################
+define('_config_quickinfo', 'Краткие сведения');
+define('_quickinfo_more', '->Подробнее<-');
+define('_quickinfo_head', 'Quickinfo - AddOn by <a href="https://github.com/BlueTeck/dzcp-quickinfobox/">BlueTeck</a>');
+define('_quickinfo_add_head', 'Добавить Краткие сведения');
+define('_quickinfo_edit_head', 'Изменить Краткие сведения');
+define('_quickinfo_added', 'Краткие сведения добавлены');
+define('_quickinfo_deleted', 'Краткие сведения удалены');
+define('_quickinfo_titel', 'Название Краткого сведения');
+define('_quickinfo_admin_show_set', 'Краткое сведеня переключить видимой');
+define('_quickinfo_admin_show_unset', 'Краткое сведеня переключить невидимой');
+define('_quickinfo_admin_status_setted', 'Краткое сведеня теперь видно!');
+define('_quickinfo_admin_status_unsetted', 'Краткое сведеня теперь невидно!');
+define('_confirm_del_quickinfo', 'Вы действительно хотите удалить Краткое сведеня');
+define('_quickinfo_edited', 'Краткое сведеня успешно обработана');
+define('_quickinfo_more_link', 'ссылка на Подробнее');
+define('_perm_quickinfo', 'Выставить Краткое сведеня');
+?>
